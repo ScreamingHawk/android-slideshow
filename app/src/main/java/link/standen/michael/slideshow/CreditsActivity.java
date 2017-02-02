d013c5949d6f64fafe26bebe354b7f316e5e4f6e
Michael Standen <screaminghawk@gmail.com>
2017-02-03 10:25:29 +1300

    Linkify Credits

--- app/src/main/java/link/standen/michael/slideshow/CreditsActivity.java
+++ app/src/main/java/link/standen/michael/slideshow/CreditsActivity.java
@@ -2,6 +2,8 @@ package link.standen.michael.slideshow;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
 
 public class CreditsActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class CreditsActivity extends AppCompatActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_credits);
 		setTitle(R.string.title_activity_credits);
+
+		// Linkify
+		((TextView)findViewById(R.id.credits_creator)).setMovementMethod(LinkMovementMethod.getInstance());
+		((TextView)findViewById(R.id.credits_content1)).setMovementMethod(LinkMovementMethod.getInstance());
+		((TextView)findViewById(R.id.credits_content2)).setMovementMethod(LinkMovementMethod.getInstance());
+		((TextView)findViewById(R.id.credits_content3)).setMovementMethod(LinkMovementMethod.getInstance());
+		((TextView)findViewById(R.id.credits_content4)).setMovementMethod(LinkMovementMethod.getInstance());
 	}
 }
